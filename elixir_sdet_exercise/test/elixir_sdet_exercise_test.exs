db2d6732dd0c4f278541e762dfce126e02994bde
Venugopal Katkam <38140662+sdetkv@users.noreply.github.com>
2020-02-20 09:54:20 -0700

    Add utility methods
    Add following utility methods
    1)  Navigate to Facebook 
    2) Selecting Item from a drop down (Mon/Day/Year)
    3)Fill form (Method to fill default values on face book register page)

--- elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
+++ elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
@@ -1,13 +1,35 @@
 defmodule ElixirSdetExerciseTest do
+  @moduledoc """
+  Tests for Facebook registration page
+  """
   # Import helpers
   use Hound.Helpers
   use ExUnit.Case
+  
+  @doc """
+  Negative tests for validationg facebook register page
+  """
+  describe "Facebook register" do 
+  
+   defp navigate() do
+    navigate_to("https://facebook.com",5)
+    IO.puts page_title()
+    assert page_title() == "Facebook - Log In or Sign Up"
+  end
 
-  # Start hound session and destroy when tests are run
-  hound_session()
+  defp select_item(element,value) do
+     execute_script("document.getElementById(\"#{element}\").value = \"#{value}\"") 
+ end 
 
-  test "goes to google" do
-    navigate_to "http://google.com"
-    IO.inspect page_title()
+  defp fill_form(form) do
+    find_within_element(form, :id, "u_0_m") |> fill_field("FirstName") #input first_name
+    find_within_element(form, :id, "u_0_o") |> fill_field("LastName")  #input last_name
+    find_within_element(form, :id, "u_0_r") |> fill_field("test@test.com")  #input email
+    find_within_element(form, :id, "u_0_w") |> fill_field("Password")  #input password
+    :timer.sleep(2000)
+    find_within_element(form, :xpath, ~s|//*[@value="1"][@type="radio"]|) |> click()  #click radiobutton
+    find_within_element(form, :id, "u_0_u") |> fill_field("test@test.com")  #input re enter email
+    :timer.sleep(8000)
+  end
   end
 end
